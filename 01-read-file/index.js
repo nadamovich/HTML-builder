e76ce9178ecdcbcab831b92c88daefaf0af0e3c7
nadamovich <nikita.adamovich@gmail.com>
2022-05-21 15:07:56 +0300

    Complete 1st task

---
+++ 01-read-file/index.js
@@ -0,0 +1,5 @@
+const path = require('path')
+const fs = require('fs')
+const textPath = path.join(__dirname, 'text.txt')
+const textReadStream = fs.createReadStream(textPath)
+textReadStream.on('data', data => console.log(data.toString()))
