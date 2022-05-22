937485eeae09d9c8de79046ff86138491741be88
nadamovich <nikita.adamovich@gmail.com>
2022-05-22 20:59:52 +0300

    complete third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const fs = require('fs')
+const {stdin, stdout} = process
+const secretFolderPath = path.join(__dirname, 'secret-folder')
+fs.readdir(secretFolderPath, {withFileTypes: true}, (err, files) => {
+    if (err) {
+        throw err
+    }
+    else {
+        files.forEach(file => {
+            if (file.isFile()){
+                const fileName = file.name.split('.')[0]
+                const fileExt = path.extname(file.name).split('.')[1]
+                const filePath = path.join(__dirname, 'secret-folder', `${file.name}`)
+                fs.stat(filePath, (err, file) => {
+                    if (err) {
+                        throw err
+                    }
+                    else {
+                        const fileSize = file.size / 1000
+                        stdout.write(`${fileName} - ${fileExt} - ${fileSize}KB\n`)
+                    }
+                }) 
+            }
+        });
+    }
+})
