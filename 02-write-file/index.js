44c372d8a5d21621eb6d7590677264dbc072f6c8
nadamovich <nikita.adamovich@gmail.com>
2022-05-22 20:04:08 +0300

    complete second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const fs = require('fs')
+const {stdin, stdout} = process
+const textPath = path.join(__dirname, 'text.txt')
+const textWriteStream = fs.createWriteStream(textPath)
+stdout.write('Hi!\n')
+stdin.on('data', data => {
+    if(data.toString().trim() !== 'exit') {
+        textWriteStream.write(data.toString())
+    }
+    else if(data.toString().trim() === 'exit') {
+        stdout.write('Good bye!\n')
+        process.exit()
+}
+})
+process.on('SIGINT', ()=> {
+    stdout.write('Good bye!\n')
+    process.exit()
+})
